496e7868886aa250a1a9147e99ebf172b7f3eb1b
logicalrzogolli <47145942+logicalrzogolli@users.noreply.github.com>
2019-11-13 15:27:59 +0100

    chapter 4 finished

--- app-server/controllers/location.js
+++ app-server/controllers/location.js
@@ -1,6 +1,31 @@
 /* GET homepage */
 const home = (req, res) => {
-    res.render('home', { title: 'HomePage' });
+    res.render('home', { 
+            title: 'Loc8r - find a place to work with wifi',
+            pageHeader: {
+                title: 'Loc8r',
+                strapline: 'Find places to work with wifi near you!'
+                },
+            locations: [{
+                    name: 'Starcups',
+                    address: '125 High Street, Reading, RG6 1PS',
+                    rating: 3,
+                    facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                    distance: '100m'
+                    },{
+                    name: 'Cafe Hero',
+                    address: '125 High Street, Reading, RG6 1PS',
+                    rating: 4,
+                    facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                    distance: '200m'
+                    },{
+                    name: 'Burger Queen',
+                    address: '125 High Street, Reading, RG6 1PS',
+                    rating: 2,
+                    facilities: ['Food', 'Premium wifi'],
+                    distance: '250m'
+                    }]
+        });
     };
 /* GET locationDetails */
 const details = (req, res) => {
