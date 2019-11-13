7f8cb478113cd38ccf4065f7927d608044200847
logicalrzogolli <47145942+logicalrzogolli@users.noreply.github.com>
2019-11-13 16:12:25 +0100

    mongodb configuration

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const app = express();
 app.set('views', path.join(__dirname,'app-server','views'));
 app.set('view engine', 'pug');
 
+// mongoose
+require('./app-server/models/db');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
