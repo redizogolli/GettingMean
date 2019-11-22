0951099b72ae196b07a2d80ea1ebdc2b9623f1f0
logicalrzogolli <47145942+logicalrzogolli@users.noreply.github.com>
2019-11-22 17:39:54 +0100

    not finished put method

--- app-api/controllers/locations.js
+++ app-api/controllers/locations.js
@@ -65,8 +65,45 @@ const locationsReadOne = (req, res) => {
  };
 
 const locationsUpdateOne = (req, res) => {
-    res.status(200).json({"status" : "success"});
- };
+    Loc.findById(req.params.locationid)
+        .exec((err, location) => {
+            if (!location) {
+                return res
+                .status(404)
+                .json({
+                "message": "location not found"
+                });
+            }
+            else if (err) {
+                return res
+                .status(404)
+                .json(err);
+            }
+                // update document
+                location.update({
+                    name: req.body.name,
+                    address: req.body.address,
+                    facilities: req.body.facilities.split(","),
+                    coords: [
+                        parseFloat(req.body.lng),
+                        parseFloat(req.body.lat)
+                    ],
+                    openingTimes:[{
+                        days: req.body.days2,
+                        opening: req.body.opening2,
+                        closing: req.body.closing2,
+                        closed: req.body.closed2,
+                    }] 
+                }, (err,updatedLocation) => {
+                     if (err) {
+                            res.status(400).json(err);
+                        } 
+                        else {
+                            res.status(201).json(updatedLocation);
+                        }
+                });
+            })
+            };
 
 const locationsDeleteOne = (req, res) => { 
     res.status(200).json({"status" : "success"});
